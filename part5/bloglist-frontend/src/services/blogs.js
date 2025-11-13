cf9be58c1fafe0083b22817278225b1c6b084a8b
Dilara <50795069+aralids@users.noreply.github.com>
2025-11-13 13:06:02 +0100

    Create blogs.js

---
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+const baseUrl = "/api/blogs";
+
+let token = null;
+
+const setToken = (newToken) => {
+	token = `Bearer ${newToken}`;
+};
+
+const getAll = () => {
+	const request = axios.get(baseUrl);
+	return request.then((response) => response.data);
+};
+
+const create = async (newObject) => {
+	const config = {
+		headers: { Authorization: token },
+	};
+
+	const response = await axios.post(baseUrl, newObject, config);
+	return response.data;
+};
+
+const updateLikes = async (newObject) => {
+	const response = await axios.put(baseUrl + "/" + newObject.id, {
+		...newObject,
+		likes: newObject.likes + 1,
+	});
+	return response.data;
+};
+
+const remove = async (id) => {
+	const config = {
+		headers: { Authorization: token },
+	};
+
+	const response = await axios.delete(baseUrl + "/" + id, config);
+	return response.data;
+};
+
+export default { getAll, create, setToken, updateLikes, remove };
